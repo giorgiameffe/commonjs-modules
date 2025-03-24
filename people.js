6b4af7da3955582cf2580f55fd4cfbac0a6c3135
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-24 17:01:18 +0100

    feat: complete third exercise

--- people.js
+++ people.js
@@ -1,12 +1,24 @@
 // 1. Importa la tua funzione da names.js
 
 const createName = require('./names.js');
-console.log(createName('Giorgia','Meffe'));
+// console.log(createName('Giorgia','Meffe'));
 
 // 2. Importa la tua funzione da hobbies.js
 
 const createHobbiesList = require('./hobbies.js');
-console.log(createHobbiesList('horseriding', 'painting', 'boardgames'))
+// console.log(createHobbiesList('horseriding', 'painting', 'boardgames'))
 
-// 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+// 3. Crea una funzione che non ha parametri.
+// La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. 
+// All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
 
+function personalInfo () {
+
+    return {
+        fullName: createName('Giorgia', 'Meffe'),
+        hobbies: createHobbiesList('horseriding', 'painting', 'boardgames')
+    }
+
+}
+
+console.log(personalInfo());
