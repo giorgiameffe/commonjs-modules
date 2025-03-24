f97c45aafe7959e34293c8e607ed99400c6dd797
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-24 16:22:08 +0100

    feat: complete first exercise

---
+++ names.js
@@ -0,0 +1,16 @@
+// 1. Crea una funzione che accetta due parametri: firstName, lastName. 
+// La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+
+function createName (firstName, lastName) {
+
+    return {
+        firstName,
+        lastName
+    }
+}
+
+console.log(createName('Giorgia','Meffe'));
+
+// 2. Esporta la funzione dal file.
+
+module.exports = createName;
